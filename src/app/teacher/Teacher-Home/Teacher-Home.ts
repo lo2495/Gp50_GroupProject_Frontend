a5bc77f58eadedf0f14f0ecc862518d05bbc1103
hahalty <lautszyin1111@gmail.com>
2023-11-16 18:14:12 +0800

    zxvz
    asdf

--- src/app/teacher/Teacher-Home/Teacher-Home.ts
+++ src/app/teacher/Teacher-Home/Teacher-Home.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
 
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog } from '@angular/material/dialog';
+import { AddTeacherDialogComponent } from 'src/component/addteacher-dialog-component/addteacher-dialog.component';
+import { AddStudentDialogComponent } from 'src/component/addStudent-dialog-component/addStudent-dialog.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-teacher-home',
   templateUrl: './Teacher-Home.html',
@@ -7,4 +12,64 @@ import { Component } from '@angular/core';
 })
 export class TeacherHome {
 
+
+
+  totalStudents!: number;
+  totalTeachers!: number;
+  constructor(
+    private http: HttpClient,
+    private dialog: MatDialog,
+    private router: Router) { }
+
+  ngOnInit(): void {
+    this.fetchTotalStudents();
+    this.fetchTotalTeachers();
+  }
+
+  fetchTotalStudents() {
+    this.http.get<any>('http://localhost:5000/api/students/count').subscribe(
+      (response: any) => {
+        this.totalStudents = response.total_students;
+      },
+      (error) => {
+        console.error('Error fetching total students:', error);
+      }
+    );
+  }
+
+  fetchTotalTeachers() {
+    this.http.get<any>('http://localhost:5000/api/teachers/count').subscribe(
+      (response: any) => {
+        this.totalTeachers = response.total_teachers;
+      },
+      (error) => {
+        console.error('Error fetching total teachers:', error);
+      }
+    );
+  }
+
+  addTeacher() {
+    const dialogRef = this.dialog.open(AddTeacherDialogComponent, {
+      width: '60%',
+      height: '70%'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
+
+  addStudent(){
+    const dialogRef = this.dialog.open(AddStudentDialogComponent, {
+      width: '60%',
+      height: '50%'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+    });
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
+
